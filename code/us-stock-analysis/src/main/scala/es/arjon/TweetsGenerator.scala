c3756480b323e13dc45994239f3c675ad38d31a6
Luciano Hammoe <luciano@MacBook-Pro-de-Luciano.local>
2017-12-18 11:30:06 -0300

    Clases tweeter

---
+++ code/us-stock-analysis/src/main/scala/es/arjon/TweetsGenerator.scala
@@ -0,0 +1,71 @@
+package es.arjon
+
+import java.time.ZonedDateTime
+import java.util.Properties
+
+object TweetsGenerator extends App {
+  val rnd = new scala.util.Random(42)
+  // This is when Dataset ends
+  var tradingBeginOfTime = ZonedDateTime.parse("2017-11-11T10:00:00Z")
+
+  if (args.length < 2) {
+    System.err.println(
+      s"""
+         |Usage: TweetsGenerator <brokers> <topics>
+         |  <brokers> is a list of one or more Kafka brokers
+         |  <topic> one kafka topic to produce to
+         |
+         |  TweetsGenerator kafka:9092 stocks
+        """.stripMargin)
+    System.exit(1)
+  }
+
+  val Array(brokers, topic) = args
+
+  println(
+    s"""
+       |Consuming tweets $brokers/$topic
+    """.stripMargin)
+
+  val spark = SparkSession.
+    builder.
+    appName("Tweets:ETL").
+    getOrCreate()
+
+  val props = new Properties()
+  props.put("bootstrap.servers", brokers)
+  props.put("client.id", "FakeStockPriceGenerator")
+  props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+  props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+
+  val propsAuth = new Properties()
+  propsAuth.put("consumerKey", "s6wI5gsn4qV3OnlKFD0HEYMXF")
+  propsAuth.put("consumerSecret", "zkngW66O6ojT5P5Q2xqGVvqltl8qwqq9vCCZLt5XLxzkbxPQlK")
+  propsAuth.put("accessToken", "859902580994052096-lSzW6HMF0KYH3bVWiVm0EjeImoSSKlj")
+  propsAuth.put("accessTokenSecret", "NdXq3sR05mm05QoMhRZOe6Z3cc8O2o4vcH0z4KRqNhVkF")
+
+
+
+
+
+
+  val twitterStream = new TwitterStream(consumerKey, consumerSecret, accessToken, accessTokenSecret, path, savingInterval, filtersTrack,filtersLocations,filtersLanguages,filtersHashtags)
+
+  twitterStream.start()
+  try{
+    while (!twitterStream.isDownloading && twitterStream.exception == null) {
+      Thread.sleep(100)
+    }
+    if (twitterStream.exception != null) {
+      throw twitterStream.exception
+    }
+  }catch{
+    case e: Exception => twitterStream.stop()
+  }
+
+  Thread.sleep(10000)
+
+  twitterStream.stop()
+
+  spark.stop()
+}
---
+++ code/us-stock-analysis/src/main/scala/es/arjon/TwitterStream.scala
@@ -0,0 +1,216 @@
+package es.arjon
+
+import java.io.{BufferedReader, File, FileNotFoundException, InputStream, InputStreamReader}
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+import java.util.{Base64, Properties}
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
+
+import ar.BH.TweetsGenerator.{props, topic}
+
+import scala.collection.JavaConverters._
+
+class TwitterStream(
+                     propsKaProperties,
+                     path: String,
+                     savingInterval: Long,
+                     filtersTrack: Array[String],
+                     filtersLocations: String,
+                     filtersLanguages: String,
+                     filtersHashtags: String) {
+
+  private val threadName = "tweet-downloader"
+  val producer = new KafkaProducer[String, String](props)
+
+  {
+    // Throw an exception if there is already an active stream.
+    // We do this check at here to prevent users from overriding the existing
+    // TwitterStream and losing the reference of the active stream.
+    val hasActiveStream = Thread.getAllStackTraces().keySet().asScala.map(_.getName).contains(threadName)
+    if (hasActiveStream) {
+      throw new RuntimeException(
+        "There is already an active stream that writes tweets to the configured path. " +
+          "Please stop the existing stream first (using twitterStream.stop()).")
+    }
+  }
+
+  @volatile private var thread: Thread = null
+  @volatile private var isStopped = false
+  @volatile var isDownloading = false
+  @volatile var exception: Throwable = null
+
+  private var httpclient: CloseableHttpClient = null
+  private var input: InputStream = null
+  private var httpGet: HttpGet = null
+
+  private def encode(string: String): String = {
+    URLEncoder.encode(string, StandardCharsets.UTF_8.name)
+  }
+
+  def start(): Unit = synchronized {
+
+
+    isDownloading = false
+    isStopped = false
+    thread = new Thread(threadName) {
+      override def run(): Unit = {
+        httpclient = HttpClients.createDefault()
+        try {
+          requestStream(httpclient)
+        } catch {
+          case e: Throwable => exception = e
+        } finally {
+          TwitterStream.this.stop()
+        }
+      }
+    }
+    thread.start()
+  }
+
+  private def requestStream(httpclient: CloseableHttpClient): Unit = {
+    val url = "https://stream.twitter.com/1.1/statuses/filter.json"
+    val timestamp = System.currentTimeMillis / 1000
+    val nonce = timestamp + scala.util.Random.nextInt
+    val oauthNonce = nonce.toString
+    val oauthTimestamp = timestamp.toString
+
+    val oauthHeaderParams = List(
+      "oauth_consumer_key" -> encode(consumerKey),
+      "oauth_signature_method" -> encode("HMAC-SHA1"),
+      "oauth_timestamp" -> encode(oauthTimestamp),
+      "oauth_nonce" -> encode(oauthNonce),
+      "oauth_token" -> encode(accessToken),
+      "oauth_version" -> "1.0"
+    )
+    // Parameters used by requests
+    // See https://dev.twitter.com/streaming/overview/request-parameters for a complete list of available parameters.
+    val requestParams1 = List("track" -> encode(filtersTrack.mkString(",")))
+    val requestParams2 = List("locations" -> encode(filtersLocations))
+    val requestParams3 = List("language" -> encode(filtersLanguages))
+
+    val parameters = (oauthHeaderParams ++ requestParams1 ++ requestParams2 ++ requestParams3).sortBy(_._1).map(pair => s"""${pair._1}=${pair._2}""").mkString("&")
+    println(parameters)
+    val base = s"GET&${encode(url)}&${encode(parameters)}"
+    val oauthBaseString: String = base.toString
+    val signature = generateSignature(oauthBaseString)
+    val oauthFinalHeaderParams = oauthHeaderParams ::: List("oauth_signature" -> encode(signature))
+    val authHeader = "OAuth " + ((oauthFinalHeaderParams.sortBy(_._1).map(pair => s"""${pair._1}="${pair._2}"""")).mkString(", "))
+
+    httpGet = new HttpGet(s"https://stream.twitter.com/1.1/statuses/filter.json?${(requestParams1 ++ requestParams2 ++ requestParams3).map(pair => s"""${pair._1}=${pair._2}""").mkString("&")}")
+    httpGet.addHeader("Authorization", authHeader)
+    println("Downloading tweets!")
+    val response = httpclient.execute(httpGet)
+    val entity = response.getEntity()
+    input = entity.getContent()
+    //println(entity)
+    //println(input)
+    if (response.getStatusLine.getStatusCode != 200) {
+      throw new RuntimeException(IOUtils.toString(input, StandardCharsets.UTF_8))
+    }
+    isDownloading = true
+    val reader = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8))
+    //def foo: String = //= Stream.continually(reader.readLine()).takeWhile(_ != null)
+
+    var line: String = null
+    var lineno = 1
+    line = reader.readLine()
+    def foo: String = line
+    println(foo)
+    var lastSavingTime = System.currentTimeMillis()
+    val s = new StringBuilder()
+    while (line != null && !isStopped) {
+      lineno += 1
+      line = reader.readLine()
+      if ((filtersHashtags == "si" && line.contains("hashtags\":[{")) || (filtersHashtags == "no")) {
+        s.append(line + "\n")
+        //display(s)
+        val now = System.currentTimeMillis()
+        if (now - lastSavingTime >= savingInterval) {
+          val file = new File(path, now.toString).getAbsolutePath
+          println("saving to " + file)
+          dbutils.fs.put(file, s.toString, true)
+          sendToKafka.process(spark,s.toString())
+          lastSavingTime = now
+          s.clear()
+        }
+      }
+    }
+  }
+
+  private def generateSignature(data: String): String = {
+    val mac = Mac.getInstance("HmacSHA1")
+    val oauthSignature = encode(consumerSecret) + "&" + encode(accessTokenSecret)
+    val spec = new SecretKeySpec(oauthSignature.getBytes, "HmacSHA1")
+    mac.init(spec)
+    val byteHMAC = mac.doFinal(data.getBytes)
+    return Base64.getEncoder.encodeToString(byteHMAC)
+  }
+
+  def stop(): Unit = synchronized {
+    isStopped = true
+    isDownloading = false
+    try {
+      if (httpGet != null) {
+        httpGet.abort()
+        httpGet = null
+      }
+      if (input != null) {
+        input.close()
+        input = null
+      }
+      if (httpclient != null) {
+        httpclient.close()
+        httpclient = null
+      }
+      if (thread != null) {
+        thread.interrupt()
+        thread = null
+      }
+    } catch {
+      case _: Throwable =>
+    }
+  }
+
+  object DatasetToParquet {
+    def process(spark: SparkSession, df: DataFrame, destinationFolder: String): Unit = {
+      // https://stackoverflow.com/questions/43731679/how-to-save-a-partitioned-parquet-file-in-spark-2-1
+      df.
+        write.
+        mode("overwrite").
+        partitionBy("year", "month", "day").
+        parquet(destinationFolder)
+    }
+  }
+
+  object DatasetToPostgres {
+
+    def process(spark: SparkSession, df: DataFrame): Unit = {
+      // Write to Postgres
+      val connectionProperties = new java.util.Properties
+      connectionProperties.put("user", "workshop")
+      connectionProperties.put("password", "w0rkzh0p")
+      val jdbcUrl = s"jdbc:postgresql://postgres:5432/workshop"
+
+      df.
+        drop("year", "month", "day"). // drop unused columns
+        write.
+        mode(SaveMode.Append).
+        jdbc(jdbcUrl, "stocks", connectionProperties)
+
+    }
+  }
+
+  object sendToKafka {
+
+    def process(spark: SparkSession, tweet: String): Unit = {
+
+
+      val data = new ProducerRecord[String, String](topic, null, tweet)
+
+      producer.send(data)
+
+      producer.close()
+
+    }
+  }
